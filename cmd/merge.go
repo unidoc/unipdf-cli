50e0db0c4fc0b81664bcbd02c47adfd716fe09c7
Adrian-George Bostan <adrg@epistack.com>
2018-12-17 14:33:03 +0200

    Improve merge subcommand documentation

--- cmd/merge.go
+++ cmd/merge.go
@@ -13,16 +13,29 @@ import (
 	"github.com/unidoc/unipdf/pdf"
 )
 
+const mergeCmdDesc = `Merge the provided input files and save the result to the
+specified output file.`
+
+var mergeCmdExample = fmt.Sprintf("%s\n",
+	fmt.Sprintf("%s merge output_file.pdf input_file1.pdf input_file2.pdf", appName),
+)
+
 var mergeCmd = &cobra.Command{
 	Use:                   "merge [FLAG]... OUTPUT_FILE INPUT_FILE...",
 	Short:                 "Merge PDF files",
-	Long:                  `A longer description that spans multiple lines and likely contains`,
-	Example:               "this is the example",
+	Long:                  mergeCmdDesc,
+	Example:               mergeCmdExample,
 	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := pdf.Merge(args[1:], args[0]); err != nil {
-			fmt.Printf("Error: %v\n", err)
+		outputPath := args[0]
+		inputPaths := args[1:]
+
+		if err := pdf.Merge(inputPaths, outputPath); err != nil {
+			printErr("Could not merge the input files: %s\n", err)
 		}
+
+		fmt.Printf("Successfully merged input files\n")
+		fmt.Printf("Output file saved to %s\n", outputPath)
 	},
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 3 {
@@ -36,7 +49,4 @@ var mergeCmd = &cobra.Command{
 func init() {
 	// Add current command to parent.
 	rootCmd.AddCommand(mergeCmd)
-
-	// Add flags.
-	mergeCmd.Flags().StringP("password", "p", "", "Help message for toggle")
 }
