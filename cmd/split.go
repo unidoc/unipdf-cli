7f02630fb0452519081dfda73a79c66d984d2211
Adrian-George Bostan <adrg@epistack.com>
2018-12-17 18:27:54 +0200

    Improve split subcommand documentation

--- cmd/split.go
+++ cmd/split.go
@@ -13,32 +13,56 @@ import (
 	"github.com/unidoc/unipdf/pdf"
 )
 
+const splitCmdDesc = `Split PDF files.
+
+The command is used to extract one or more page ranges from the input file
+and save the result as the output file.
+If no page range is specified, all the pages from the input file will be
+copied to the output file.
+
+An example of the pages parameter: 1-3,4,6-7
+Only pages 1,2,3 (1-3), 4 and 6,7 (6-7) will be present in the output file,
+while page number 5 is skipped.
+`
+
+var splitCmdExample = fmt.Sprintf("%s\n%s\n",
+	fmt.Sprintf("%s split input_file.pdf output_file.pdf 1-2", appName),
+	fmt.Sprintf("%s split -p pass input_file.pd output_file.pdf 1-2,4", appName),
+)
+
 // splitCmd represents the split command
 var splitCmd = &cobra.Command{
-	Use:                   "split [FLAG]... INPUT_FILE PAGES OUTPUT_FILE",
+	Use:                   "split [FLAG]... INPUT_FILE OUTPUT_FILE [PAGES]",
 	Short:                 "Split PDF files",
-	Long:                  `A longer description that spans multiple lines and likely contains`,
-	Example:               "this is the example",
+	Long:                  splitCmdDesc,
+	Example:               splitCmdExample,
 	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		inputPath := args[0]
-		outputPath := args[2]
+		outputPath := args[1]
 		password, _ := cmd.Flags().GetString("password")
 
-		pages, err := parsePageRange(args[1])
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-			return
+		// Parse page range.
+		var err error
+		var pages []int
+
+		if len(args) > 2 {
+			if pages, err = parsePageRange(args[2]); err != nil {
+				printUsageErr(cmd, "Invalid page range specified\n")
+			}
 		}
 
 		err = pdf.Split(inputPath, outputPath, password, pages)
 		if err != nil {
-			fmt.Printf("Error: %v\n", err)
+			printErr("Error: %v\n", err)
 		}
+
+		fmt.Printf("Successfully split file %s\n", inputPath)
+		fmt.Printf("Output file saved to %s\n", outputPath)
 	},
 	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 3 {
-			return errors.New("Must provide the input file, page range and output file\n")
+		if len(args) < 2 {
+			return errors.New("Must provide at least the input and output files\n")
 		}
 
 		return nil
