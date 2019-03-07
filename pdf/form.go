8237cbf41896d03895d3061c02d84608e62359da
Adrian-George Bostan <adrg@epistack.com>
2019-03-07 22:30:59 +0200

    Add fdfmerge subcommand

--- pdf/form.go
+++ pdf/form.go
@@ -7,6 +7,7 @@ package pdf
 
 import (
 	"github.com/unidoc/unidoc/pdf/annotator"
+	"github.com/unidoc/unidoc/pdf/fdf"
 	"github.com/unidoc/unidoc/pdf/fjson"
 	unipdf "github.com/unidoc/unidoc/pdf/model"
 )
@@ -25,52 +26,36 @@ func FormExport(inputPath string) (string, error) {
 	return fieldData.JSON()
 }
 
-// FormFill fills the form field values from the PDF file specified by the
+// FormFillJSON fills the form field values from the PDF file specified by the
 // inputPath parameter, using the values from the JSON file specified by the
 // jsonPath parameter. The output PDF file is saved at the location specified
 // by the outputPath parameter. The output file form annotations can be
 // flattened by using the flatten parameter.
 // A password can be specified for encrypted input files.
-func FormFill(inputPath, jsonPath, outputPath, password string, flatten bool) error {
+func FormFillJSON(inputPath, jsonPath, outputPath, password string, flatten bool) error {
 	// Read JSON field data.
 	fieldData, err := fjson.LoadFromJSONFile(jsonPath)
 	if err != nil {
 		return err
 	}
 
-	// Read input file.
-	r, _, _, _, err := readPDF(inputPath, password)
+	return formFill(inputPath, fieldData, outputPath, password, flatten)
+}
+
+// FormFillFDF fills the form field values from the PDF file specified by the
+// inputPath parameter, using the values from the FDF file specified by the
+// jsonPath parameter. The output PDF file is saved at the location specified
+// by the outputPath parameter. The output file form annotations can be
+// flattened by using the flatten parameter.
+// A password can be specified for encrypted input files.
+func FormFillFDF(inputPath, fdfPath, outputPath, password string, flatten bool) error {
+	// Read field data.
+	fieldData, err := fdf.LoadFromPath(fdfPath)
 	if err != nil {
 		return err
 	}
 
-	// Populate the form data.
-	if err = r.AcroForm.Fill(fieldData); err != nil {
-		return err
-	}
-
-	// Flatten form.
-	if flatten {
-		fieldAppearance := annotator.FieldAppearance{
-			OnlyIfMissing:        true,
-			RegenerateTextFields: true,
-		}
-
-		if err = r.FlattenFields(true, fieldAppearance); err != nil {
-			return err
-		}
-		r.AcroForm = nil
-	}
-
-	// Copy input file contents.
-	w := unipdf.NewPdfWriter()
-	if err := readerToWriter(r, &w, nil); err != nil {
-		return err
-	}
-
-	// Save output file.
-	safe := inputPath == outputPath
-	return writePDF(outputPath, &w, safe)
+	return formFill(inputPath, fieldData, outputPath, password, flatten)
 }
 
 // FormFlatten flattens all the form annotation from the PDF file specified by
@@ -93,6 +78,42 @@ func FormFlatten(inputPath, outputPath, password string) error {
 		return err
 	}
 	r.AcroForm = nil
+
+	// Copy input file contents.
+	w := unipdf.NewPdfWriter()
+	if err := readerToWriter(r, &w, nil); err != nil {
+		return err
+	}
+
+	// Save output file.
+	safe := inputPath == outputPath
+	return writePDF(outputPath, &w, safe)
+}
+
+func formFill(inputPath string, provider unipdf.FieldValueProvider, outputPath, password string, flatten bool) error {
+	// Read input file.
+	r, _, _, _, err := readPDF(inputPath, password)
+	if err != nil {
+		return err
+	}
+
+	// Populate the form data.
+	if err = r.AcroForm.Fill(provider); err != nil {
+		return err
+	}
+
+	// Flatten form.
+	if flatten {
+		fieldAppearance := annotator.FieldAppearance{
+			OnlyIfMissing:        true,
+			RegenerateTextFields: true,
+		}
+
+		if err = r.FlattenFields(true, fieldAppearance); err != nil {
+			return err
+		}
+		r.AcroForm = nil
+	}
 
 	// Copy input file contents.
 	w := unipdf.NewPdfWriter()
